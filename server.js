de31da9d3a344ba14ed28b3df79c3c43791a89be
Yashwardhan Pauranik <yash@appointy.com>
2018-05-05 17:28:19 +0530

    setup start script and port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 let app = express();
+let port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -47,4 +48,4 @@ app.get('/bad', (req, res) => {
         });
 });
 
-app.listen(3000, () => console.log('Server is up'));
+app.listen(port, () => console.log('Server is up' + port));
